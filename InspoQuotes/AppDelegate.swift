615d562a37d02cd8fa4b29bad80dc5011eba86ce
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-10-19 14:09:44 +0700

    11

--- InspoQuotes/AppDelegate.swift
+++ InspoQuotes/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  InspoQuotes
 //
-//  Created by Angela Yu on 18/08/2019.
+//  Created by D i on 18/08/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
